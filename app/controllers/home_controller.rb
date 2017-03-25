922d8ecdb72dee6674d2dbfed86714bdd039ec7d
Mike Rogers <me@mikerogers.io>
2017-03-24 22:25:58 -0600

    Adding in kml

--- app/controllers/home_controller.rb
+++ app/controllers/home_controller.rb
@@ -2,4 +2,8 @@ class HomeController < ApplicationController
   def index
     #Meetup::FindEvents.nearby
   end
+
+  def kml
+    respond_to :kml
+  end
 end
--- config/routes.rb
+++ config/routes.rb
@@ -2,4 +2,6 @@ Rails.application.routes.draw do
   
 
   root controller: :home, action: :index
+
+  get :kml, controller: :home, action: :kml, defaults: { format: :kml }
 end
