26cde79a478daa628446e1cdff2c4017f11325a3
Mike Rogers <me@mikerogers.io>
2017-03-25 17:01:52 -0600

    correcting caching servers in production

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -51,9 +51,9 @@ Rails.application.configure do
 
   # Use a different cache store in production.
   config.cache_store = :dalli_store,
-    (ENV["MEMCACHIER_SERVERS"] || "").split(","),
-    {:username => ENV["MEMCACHIER_USERNAME"],
-     :password => ENV["MEMCACHIER_PASSWORD"],
+    (ENV["MEMCACHEDCLOUD_SERVERS"] || "").split(","),
+    {:username => ENV["MEMCACHEDCLOUD_USERNAME"],
+     :password => ENV["MEMCACHEDCLOUD_PASSWORD"],
      :failover => true,
      :socket_timeout => 1.5,
      :socket_failure_delay => 0.2,
