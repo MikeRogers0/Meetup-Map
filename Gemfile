26b895b204c369d6ff85cbd01bacedd7df089245
Mike Rogers <me@mikerogers.io>
2018-07-28 22:02:36 +0100

    Keeping the DB consistent

--- Gemfile
+++ Gemfile
@@ -7,7 +7,7 @@ git_source(:github) do |repo_name|
   "https://github.com/#{repo_name}.git"
 end
 
-gem 'dotenv-rails', groups: %i[development test]
+gem 'dotenv-rails'
 
 # Bundle edge Rails instead: gem 'rails', github: 'rails/rails'
 gem 'rails', '~> 5.0'
--- config/deploy.rb
+++ config/deploy.rb
@@ -19,7 +19,7 @@ set :deploy_to, -> { "/home/ubuntu/var/www/#{fetch(:application)}" }
 set :pty, true
 
 # Default value for :linked_files is []
-append :linked_files, 'config/puma.rb', '.env'
+append :linked_files, 'config/puma.rb', '.env', 'config/database.yml'
 
 # Default value for linked_dirs is []
 # append :linked_dirs, "log", "tmp/pids", "tmp/cache", "tmp/sockets", "public/system"
